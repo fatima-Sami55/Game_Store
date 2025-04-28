a32a5d5628120981d972061570db68ae48ea0c5b
FatimaSami <84532632+fatima-Sami55@users.noreply.github.com>
2025-04-28 22:39:59 +0500

    commiting login and register pages

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -2,7 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/create', (req, res) => {
+router.get('/login', (req, res) => {
+  res.render("login");
+})
+
+router.post('/login', (req, res) => {
+  
+})
+
+router.get('/register', (req, res) => {
+    res.render("register");
+})
+
+router.post('/register', (req, res) => {
+    const { name, email, phone, password } = req.body;
+    console.log('Registering:', name, email, phone, password);
+
+    res.send(`User ${name} registered successfully!`);
+})
+
+router.get('/logout', (req, res) => {
   
 })
 
